9b9f3b3eee4c5299a52a5810fe4a47e4c456ee42
Zak <andresjr195@hotmail.com>
2022-06-25 17:05:17 -0500

    update

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2500,
+    duration: 1500,
     delay: 300,
     // reset: true,
 })
 
-sr.reveal(`.journey`)
+sr.reveal(`.journey, .seccion_1, .container-explorer, .vacation-container`)
 sr.reveal(`.home__handle`, {delay: 50})
 sr.reveal(`.pick`, {delay: 600, origin: 'bottom'})
