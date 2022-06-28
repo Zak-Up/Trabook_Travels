f367421391b4bef757de85cf6f576926dc8d243d
Zak <andresjr195@hotmail.com>
2022-06-27 21:45:29 -0500

    update

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ const sr = ScrollReveal({
     // reset: true,
 })
 
-sr.reveal(`.journey, .seccion_1, .container-explorer, .vacation-container, .section2J`)
+sr.reveal(`.journey, .seccion_1, .container-explorer, .vacation-container, .section2J, .deals, .updates`)
 sr.reveal(`.home__handle`, {delay: 50})
 sr.reveal(`.pick, .subscribe-container`, {delay: 600, origin: 'bottom'})
